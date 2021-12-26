dbbb9e2f50d8998bc45c931adc37cd25b1dfe0fb
dolverson <olverson@protonmail.com>
2021-12-26 16:30:56 +0000

    Created customized websites and Dockerfiles

---
+++ user-service-api/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.json([
+	{
+  	name:'Bob',
+  	email:'Bob@gmail.com'
+	},
+	{
+        name:'Alice',
+        email:'alice@gmail.com'
+        }, 
+	{
+        name:'Tigger',
+        email:'tigger@gmail.com'
+        },
+	{
+        name:'Erin',
+        email:'Erin@gmail.com'
+        }
+]))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)})
